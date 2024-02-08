80fa50bf3bb5e568a62908b765a70a597d228cb9
ceci-00 <ceci.ga003@gmail.com>
2024-02-07 21:37:47 -0600

    adding food selections options

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,22 @@ const getDrinkByName = async (name) => {
   getDrinkByName('margarita')
     .then(data => console.log(data));
 
+                         
+const foodSelections = async () => {
+  const mealResponse = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php?');
+  const mealData = await response.json();
+  return mealData.category;
+}
+foodSelections('strCategory')
+  .then(mealData => console.log(mealData));
+
+  //  document.getElementByClass('block').style.display = 'none'
+  // randomize selection
+//  $("#lean-meat").innerText.sort(leanMeatList[])
+//  $("#poultry").innerText.sort(poultryList[])
+//  currentQuestionIndex = 0
+//  setNextQuestion()
+
+//  leanMeatList = ['Beef', 'Pork', 'Lamb', 'Goat']
+//  poultryList = ['Chicken', 'Turkey', 'Duck']
+//  seafoodList = ['Fish', 'Crab', 'Lobster', 'Mussels']
